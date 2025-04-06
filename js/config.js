77b8d6f8f50c374ccb53b0012a915849036a0f02
Anirudhtallavajhula <69448192+Anirudhtallavajhula@users.noreply.github.com>
2025-04-06 17:33:59 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Jv9dZhlfE', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '50pq29hmvl5au2admk5ajlnv0i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1b82kpgg6d6slao39306bmvrv1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
