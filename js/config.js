04a26689de6853a4d7bd37306970b52bbb7428ef
Anirudhtallavajhula <69448192+Anirudhtallavajhula@users.noreply.github.com>
2025-04-06 18:23:52 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Jv9dZhlfE', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1b82kpgg6d6slao39306bmvrv1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_Jv9dZhlfE',
+        userPoolClientId: '1b82kpgg6d6slao39306bmvrv1',
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '
-https://lar009gvp1.execute-api.us-east-1.amazonaws.com/dev
-' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://lar009gvp1.execute-api.us-east-1.amazonaws.com/dev'
     }
 };
